2459e299dec582a505a75ddbb15a625aa0a824b1
Jakob Ankarhem <jakob.ankarhem@jetshop.se>
2022-09-26 10:49:32 +0200

    trigger

--- app/session.ts
+++ app/session.ts
@@ -5,12 +5,6 @@ export const { getSession, commitSession, destroySession } =
     // a Cookie from `createCookie` or the CookieOptions to create one
     cookie: {
       name: '__session',
-
-      // all of these are optional
-      // domain: "remix.run",
-      // Expires can also be set (although maxAge overrides it when used in combination).
-      // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
-      //
       // expires: new Date(Date.now() + 60_000),
       httpOnly: true,
       // maxAge: ,
